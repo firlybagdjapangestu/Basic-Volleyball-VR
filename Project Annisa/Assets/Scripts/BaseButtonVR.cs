397c2fe3597efc1140f27f17ac7030085959bf19
firlybagdjapangestu <57309957+firlybagdjapangestu@users.noreply.github.com>
2025-04-24 19:12:51 +0700

    Penambahan Text Diatas Kepala

---
+++ Project Annisa/Assets/Scripts/BaseButtonVR.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using UnityEngine;
+
+public class BaseButtonVR : MonoBehaviour
+{
+    public AudioSource audioSource;    
+    [SerializeField] private float gazeDuration = 2.0f;
+
+    private Coroutine gazeCoroutine;
+
+    protected static AnimationClip selectedAnimation; // Menyimpan animasi yang dipilih
+    protected static Animator sharedAnimator; // Animator yang dipakai oleh semua button
+    protected static AudioClip selectedAudioClip; // Menyimpan audio yang dipilih
+    protected static string nameMove;
+
+    protected virtual void Start()
+    {
+        
+    }
+
+    protected virtual void Update()
+    {
+        if (Input.GetButtonDown("Fire1") || Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject == gameObject)
+                {
+                    OnClickTrigger();
+                }
+            }
+        }
+    }
+
+    protected virtual void OnPointerEnter()
+    {
+
+    }
+
+    protected virtual void OnPointerExit()
+    {
+
+    }
+
+
+    protected virtual void OnClickTrigger()
+    {
+        // Fungsi ini akan di-override oleh button spesifik
+    }
+
+    
+}
---
+++ Project Annisa/Assets/Scripts/ButtonChoiceAnimation.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+using TMPro;
+
+public class ButtonChoiceAnimation : BaseButtonVR
+{
+    private Renderer _myRenderer;
+    [SerializeField] private string nameMove;
+    [SerializeField] private TextMeshPro displayText;
+    [SerializeField] private Material InactiveMaterial;
+    [SerializeField] private Material GazedAtMaterial;
+    [SerializeField] private PlayableDirector playableDirector;
+    [SerializeField] private TimelineAsset timelineAsset;
+    [SerializeField] AudioClip audioClip;
+
+
+    protected override void Start()
+    {
+        base.Start();
+        _myRenderer = GetComponent<Renderer>();
+    }
+
+    protected override void OnPointerEnter()
+    {
+        SetMaterial(true);
+        base.OnPointerEnter();
+    }
+
+    protected override void OnPointerExit()
+    {
+        SetMaterial(false);
+        base.OnPointerExit();
+    }
+
+    private void SetMaterial(bool gazedAt)
+    {
+        if (InactiveMaterial != null && GazedAtMaterial != null)
+        {
+            _myRenderer.material = gazedAt ? GazedAtMaterial : InactiveMaterial;
+        }
+    }
+
+    protected override void OnClickTrigger()
+    {
+        base.OnClickTrigger();
+        print("Clicked on ");
+        ChangeDisplayText();
+        PlayAudio();
+        PlayTimeline();
+    }
+
+    private void PlayTimeline()
+    {
+        if (playableDirector != null && timelineAsset != null)
+        {
+            playableDirector.playableAsset = timelineAsset;
+            playableDirector.Play();
+        }
+    }
+
+    private void PlayAudio()
+    {
+        if (audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+
+        if (audioClip != null)
+        {
+            audioSource.clip = audioClip;
+            audioSource.Play();
+        }
+    }
+
+    private void ChangeDisplayText()
+    {
+        if (displayText != null)
+        {
+            displayText.text = nameMove;
+        }
+    }
+}
